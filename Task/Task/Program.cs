012694b580da07c8eee40f3f6e85d25287034300
anarfarzaliyev9 <anarfarzaliyev9@gmail.com>
2020-05-05 23:37:50 +0400

    add

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -16,77 +16,15 @@ namespace Task
             Invoice invoice = new Invoice("678904", "Alex", "Foxtrot")
             {
                 Article = "USB-hab",
-                Quantity = "2"
+                Quantity = "6"
             };
             
-            Console.WriteLine(invoice.CostCalculation(false));
+            Console.WriteLine($"Price is: {invoice.CostCalculation(true)} AZN");
             
             Console.ReadKey();
         }
     }
 
-    class Invoice
-    {
-        private string account;
-        private string customer;
-        private string provider;
-        private double edv=0.18;
-        private double productPrice;
-        private string _article;
-        private double productPriceEdv;
-        public Invoice(string account, string customer, string provider)
-        {
-            this.account = account;
-            this.customer = customer;
-            this.provider = provider;
-        }
-        public string Quantity { get; set; }
-
-      
-        public string Article 
-        {
-            get
-            {
-                return _article;
-            }
-            set
-            {
-                _article = value;
-                switch(_article.ToLower())
-                {
-                    case "laptop":
-                         productPrice=1400;
-                        break;
-                    case "sd-cadr":
-                            productPrice=30;
-                        break;
-                    case "usb-hab":
-                          productPrice=12;
-                        break;
-                    default:
-                         productPrice=0;
-                        break;
-
-                }
-            }
-        }
-       
-        public double CostCalculation(bool needEdv)
-        {
-            if(needEdv)
-            {
-                productPriceEdv = (productPrice * int.Parse(Quantity)) + productPrice * int.Parse(Quantity) * edv;
-                return productPriceEdv;
-            }
-            else
-            {
-                productPriceEdv = productPrice * int.Parse(Quantity);
-                return productPriceEdv;
-            }
-        }
-       
-
-
-    }
+   
  
 }
