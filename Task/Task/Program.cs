e4ccb142bd6abb4dd3e18b89ec3b8aa0625adb2e
anarfarzaliyev9 <anarfarzaliyev9@gmail.com>
2020-05-05 22:41:48 +0400

    change

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -18,4 +19,63 @@ namespace Task
             Console.ReadKey();
         }
     }
+
+    class Invoice
+    {
+        private string account;
+        private string customer;
+        private string provider;
+        private double edv=0.18;
+        private double productPrice;
+        private string _article;
+        public string Article 
+        {
+            get
+            {
+                return _article;
+            }
+            set
+            {
+                switch(value)
+                {
+                    case "laptop":
+                         productPrice=1400;
+                        break;
+                    case "sd-cadr":
+                            productPrice=30;
+                        break;
+                    case "usb-hab":
+                          productPrice=12;
+                        break;
+                    default:
+                         productPrice=0;
+                        break;
+
+                }
+            }
+        }
+        public string Quantity { get; set; }
+
+        public Invoice (string account,string customer,string provider )
+	{
+            this.account=account;
+            this.customer=customer;
+            this.provider=provider;
+	}
+
+        public void CostCalculation(bool needEdv)
+        {
+            if(needEdv)
+            {
+            
+            }
+            else
+            {
+
+            }
+        }
+
+
+    }
+   
 }
